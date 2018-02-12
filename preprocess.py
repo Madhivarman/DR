5084dca4e560b4d05655adf07da48ac8dd7f6e37
Madhivarman <madhivarman451@gmail.com>
2018-02-12 23:14:03 +0530

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+
+#global declaration
+path_file = "/home/madhi/Documents/python programs/neuralnetworks/fp/Reuters21578-Apte-115Cat/training"
+topic_folders = os.listdir(path_file)
+
+test_path_file = "/home/madhi/Documents/python programs/neuralnetworks/fp/Reuters21578-Apte-115Cat/training"
+test_topic_folders = os.listdir(test_path_file)
+
+#file to change into dataframe
+
+"""def createDataFrame(file):
+	read_file = pd.read_csv(file,delimiter="\n",header=None)
+	dataframe = read_file.to_csv(file+".csv")
+	return dataframe"""
+
+def assignIdToDocuments(filename,topic_details_list,folder_path):
+
+	doc_id = 0 #initial declaration
+	with open(filename,"w") as fp:
+		#opening the project folders
+		for folders in folder_path:
+			no_of_docs = os.listdir(path_file+"/"+folders)
+			#iterate through the number of documents
+			for org_docs in no_of_docs:
+				with open(path_file + "/" + folders +"/" + org_docs,"r") as f:
+					data = f.read()
+					fp.write(str(doc_id)+","+data)
+					fp.write("\n")
+
+			doc_id += 1
+
+
+
+#main function
+if __name__ == '__main__':
+	#dataset
+	index_file  = 'index.txt'
+
+	#index_file_df = createDataFrame(index_file)
+
+	#creating a trainingdataset for document classification
+	topic_names = [] #list to save topic folders
+	topic_id = 0
+
+	for topics in topic_folders:
+		topic_names.append((topic_id,topics))
+		topic_id += 1
+
+	topic_names = sorted(topic_names)
+	topic_details = dict(topic_names)
+
+	print(topic_names)
+
+	#assign documents with respective id's
+	assign_id_to_docs_train = assignIdToDocuments("trainingdataset.txt",topic_names,topic_folders)
+	assign_id_to_docs_test = assignIdToDocuments("testdataset.txt",topic_names,test_topic_folders)
