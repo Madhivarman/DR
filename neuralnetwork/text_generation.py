efcd3035af6b545b13779ee860a834a5c9d05ae7
Madhivarman <madhivarman451@gmail.com>
2018-03-09 21:31:31 +0530

    Text Generation using  Keras

--- neuralnetwork/text_generation.py
+++ neuralnetwork/text_generation.py
@@ -1,72 +1,81 @@
-import tensorflow as tf 
-import numpy as np 
+#sequence generating model
+"""In constructing language model ,
+   1WI1WO -> 58%
+   LBL -> 98.56 %  500 epochs
+"""
+from numpy import array
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
 
-#load the file to create a Text 
-file = '/home/madhi/Documents/python programs/neuralnetworks/fp/Reuters21578-Apte-115Cat/training/cocoa/0000000'
+# generate a sequence from a language model
+def generate_seq(model, tokenizer, max_length, seed_text, n_words):
+	in_text = seed_text
+	# generate a fixed number of words
+	for _ in range(n_words):
+		# encode the text as integer
+		encoded = tokenizer.texts_to_sequences([in_text])[0]
+		# pre-pad sequences to a fixed length
+		encoded = pad_sequences([encoded], maxlen=max_length, padding='pre')
+		# predict probabilities for each word
+		yhat = model.predict_classes(encoded, verbose=0)
+		# map predicted word index to word
+		out_word = ''
+		for word, index in tokenizer.word_index.items():
+			if index == yhat:
+				out_word = word
+				break
+		# append to input
+		in_text += ' ' + out_word
+	return in_text
 
-with open(file,'r')  as fp:
-	file_text = fp.read()
-	#convert  into lower
-	file_text = file_text.lower() #convert the file into lower case
-
-#create a set of unique characters
-chars = sorted(list(set(file_text))) #total 43 characters
-char_to_int = dict((c,i) for i,c in enumerate(chars))  #total chars 43 with their frequency in their document
-
-#int_to_char change
-int_to_char = dict((i,c) for i,c in enumerate(chars))
-
-sequence_length = 100
-n_vocab = len(chars) #length of the characters
-n_chars = len(file_text) #length of the total words in a file
-data_X = [] #dataset
-data_Y = [] #output to the dataset
-
-for i in range(0,n_chars - sequence_length,1):
-	seq_in = file_text[i:i+sequence_length] #separate the text into slices
-	seq_out = file_text[i+sequence_length] #output for the input
-
-	data_X.append([char_to_int[char]  for char in seq_in])   #append to the training data
-	data_Y.append([char_to_int[seq_out]]) #append output to the file
-
-n_patterns = len(data_X) #2798
-
-#now pick a random seed
-start = np.random.randint(0,len(data_X)-1)
-pattern = data_X[start]
-
-print("Seed")
-print("\"".join([int_to_char[value] for value in pattern]),"\"")
-
-#start the session
-#reset the defaule graph
-
-tf.reset_default_graph()
-#load the meta data graph
-imported_meta = tf.train.import_meta_graph('text_generate_trained_model.ckpt.meta')
-
-with tf.Session() as sess:
-	imported_meta.restore(sess,tf.train.latest_checkpoint('./'))
-
-	#accessing the default graph which we restored
-	graph = tf.get_default_graph()
-
-	#op that we can be processed to get the output
-	#last is the tensor that is the prediction of the network
-	y_pred = graph.get_tensor_by_name("prediction:0")
-	#generate characters
-	for i in range(500):
-		x = np.reshape(pattern,(1,len(pattern),1))
-		x = x / float(n_vocab)
-		prediction = sess.run(y_pred,feed_dict=x)
-		index = np.argmax(prediction)
-		result = int_to_char[index]
-		seq_in = [int_to_char[value] for value in pattern]
-		sys.stdout.write(result)
-		patter.append(index)
-		pattern = pattern[1:len(pattern)]
-
-	print("\n Done...!")
+# source text
+file_to = '/home/madhi/Documents/python programs/neuralnetworks/fp/Reuters21578-Apte-115Cat/training/citruspulp/cocoa/0000000'
+data = open(file_to).read()
+# prepare the tokenizer on the source text
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts([data])
+# determine the vocabulary size
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
+# create line-based sequences
+sequences = list()
+for line in data.split('\n'):
+	encoded = tokenizer.texts_to_sequences([line])[0]
+	for i in range(1, len(encoded)):
+		sequence = encoded[:i+1]
+		sequences.append(sequence)
 
 
-sess.close()
+# pad input sequences
+max_length = max([len(seq) for seq in sequences])
+sequences = pad_sequences(sequences, maxlen=max_length, padding='pre')
+
+
+# split into input and output elements
+sequences = array(sequences)
+#split the data as X,y
+X, y = sequences[:,:-1],sequences[:,-1]
+#convert the data into categorical
+y = to_categorical(y, num_classes=vocab_size)
+
+# define model
+model = Sequential()
+model.add(Embedding(vocab_size, 10, input_length=max_length-1))
+model.add(LSTM(50))
+model.add(Dense(vocab_size, activation='softmax'))
+print(model.summary())
+
+# compile network
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit network
+model.fit(X, y, epochs=500, verbose=2)
+
+
+# evaluate model
+print(generate_seq(model, tokenizer, max_length-1, 'cocoa', 20))
+print(generate_seq(model, tokenizer, max_length-1, 'middlemen',20))
